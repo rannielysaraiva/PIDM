99275224d38dbc508580b1c31c389fa7202f7e03
rannielysaraiva <rannielysaraiva@alu.ufc.br>
2021-01-09 22:56:13 -0300

    Trabalho 03

---
+++ Trabalho03/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StatusBar, View, Text, } from 'react-native';
+import IMCApp from './IMCApp';
+
+export default function App() {
+  return (
+    <View>
+      <IMCApp></IMCApp>
+    </View>  
+  );
+}
